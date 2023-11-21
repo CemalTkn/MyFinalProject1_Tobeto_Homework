868f741d3926f34d6a3fc8e2c8e9396651af29d1
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-11-21 20:58:04 +0300

    9.Gün Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,10 +3,24 @@ using Business1.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-ProductManager productManager = new ProductManager(new EfProductDal());
+ProductTest();
+//CategoryTest();
 
-foreach (var product in productManager.GetByUnitPrice(50,100))
+static void ProductTest()
 {
-    Console.WriteLine(product.ProductName);
+    ProductManager productManager = new ProductManager(new EfProductDal());
+
+    foreach (var product in productManager.GetProductDetails())
+    {
+        Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+    }
 }
 
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
+}
