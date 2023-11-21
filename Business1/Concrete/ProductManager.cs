d4f9c246454a4f6aae52ec1cf20c4c98bd81e637
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-11-21 15:33:13 +0300

    Add project files.

---
+++ Business1/Concrete/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using Business1.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business1.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
